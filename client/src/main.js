75ee100082cf9f47f6496d1bbb495c657aad937b
bjorn <bjorn.blomberg@cgi.com>
2023-06-05 12:26:03 +0200

    första steget till att kontrolara att man är anslutern just nu kan den bara pinga servern

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+var ping = require('ping');
 
 const app = express();
+
+var CONFIG = require('./config.json');
+var serverPort = CONFIG.sport
+var serverIp = CONFIG.host
+var clientPort = CONFIG.cport
 
 // Hantera statiska filer
 app.use(serveStatic('www'));
 
 // Starta servern
-const port = process.env.PORT || 3000;
+const port = clientPort;
+var hosts = [serverIp];
 app.listen(port, () => {
   console.log(`Servern körs på port ${port}`);
+  hosts.forEach(function (host) {
+    ping.promise.probe(host)
+        .then(function (res) {
+            if (res.alive == true) {
+              console.log(`conected to server on ${serverIp}:${serverPort}`);
+            }
+        });
+});
 });
