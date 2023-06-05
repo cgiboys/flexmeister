6d3245da10f7b6a88dbb211e103b4f303c14bef5
bjorn <bjorn.blomberg@cgi.com>
2023-06-05 14:56:44 +0200

    update client cheek if server alive

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Ett set att holla koll på din flex tid
 besta appen någonsin
 
 ## Klient
-Yo yo yo
+Kör Client med "nodejs src/main.js"
 
 ## Server
 Kör servern med "gradle bootRun"
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 var ping = require('ping');
+const https = require('https');
 
 const app = express();
 
@@ -25,4 +26,26 @@ app.listen(port, () => {
             }
         });
 });
+});
+
+https.get(serverIp + ":" + serverPort + "/api/alive", res => {
+  let data = [];
+  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+  console.log('Status Code:', res.statusCode);
+  console.log('Date in Response header:', headerDate);
+
+  res.on('data', chunk => {
+    data.push(chunk);
+  });
+
+  res.on('end', () => {
+    console.log('Response ended: ');
+    const users = JSON.parse(Buffer.concat(data).toString());
+
+    for(user of users) {
+      console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+    }
+  });
+}).on('error', err => {
+  console.log('Error: ', err.message);
 });
