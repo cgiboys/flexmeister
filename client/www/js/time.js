9a9634c83cceafdb7917de8ec91d8d898e6b4d0d
Bjorn <bjorn.blomberg@cgi.com>
2023-06-12 11:42:36 +0000

    Add Flex counter and style stuff

--- client/www/js/time.js
+++ client/www/js/time.js
@@ -1,6 +1,24 @@
 
 $(document).ready(function () {
+  var userId = getUserIdFromCookie();
+  if (userId != null) {
+    getUserTime(userId);
+  }
 });
+
+function getUserIdFromCookie() {
+  var cookies = document.cookie.split("; ");
+  //console.log(cookies);
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].split("=");
+    //console.log(cookie);
+    if (cookie[0] === "userId") {
+      //console.log(cookie[1]);
+      return cookie[1];
+    }
+  }
+  return null; // Om cookien inte hittas
+}
 
 function getUserTime(inUserId) {
     $.ajax({
@@ -8,17 +26,28 @@ function getUserTime(inUserId) {
       type: 'GET',
       success: function (data) {
         var tableBody = $('#time-table-body');
+        var flexTotalH1 = $('#flexTotal');
+        var felxCount = $('#flexCount');
         //var menuUserIndikater = $('#menu');
         //var userName = '<a class="menu-button userName">' + data.username + '</a>'
         tableBody.empty(); // Rensa befintligt innehåll
+        var flexTotal = 0;
         
         for (var i = 0; i < data.times.length; i++) {
+          flexTotal += data.times[i];
           var row = '<tr>' +
             '<td>' + data.times[i] + '</td>' +
             '<td>' + data.dates[i] + '</td>' +
             '</tr>';
             
           tableBody.append(row);
+        }
+        if (flexTotal > 0) {
+        flexTotalH1.text("+" + flexTotal + "H");
+        felxCount.addClass("back-green");
+        } else {
+          flexTotalH1.text("-" + flexTotal + "H");
+          felxCount.addClass("back-read");
         }
         //menuUserIndikater.append(userName);
       },
